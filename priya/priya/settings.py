9b4bbf9394dca076abc606ad619d36bf8bc83c86
23003522 <priyadivyapriyadivya9@gmail.com>
2023-07-27 09:16:15 +0000

    Success

--- priya/priya/settings.py
+++ priya/priya/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = '8mypa!sw(8$y_c!f&npyyha-2)%d5u=!qd(9lc75!rxy@$#8&h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+ os.path.join(BASE_DIR,'static')
+ ]
